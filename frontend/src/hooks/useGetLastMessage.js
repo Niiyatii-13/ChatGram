684cd6681f50f89cc1567507656fce203a571584
Niiyatii-13 <166405063+Niiyatii-13@users.noreply.github.com>
2024-07-29 00:54:26 +0530

    Update useGetLastMessage.js

--- frontend/src/hooks/useGetLastMessage.js
+++ frontend/src/hooks/useGetLastMessage.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import useConversation from "../zustand/useConversation";
 import axios from "axios";
 import toast from "react-hot-toast";
-
+import {useAuthContext} from '../context/AuthContext.jsx' 
 
 const useGetMessages = ({id}) => {
 	const [loading, setLoading] = useState(false);
