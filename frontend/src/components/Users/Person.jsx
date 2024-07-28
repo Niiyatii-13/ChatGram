abb051c565f0a4a43460422b3a131c5d25c59962
Niiyatii-13 <166405063+Niiyatii-13@users.noreply.github.com>
2024-07-29 00:52:37 +0530

    Update Person.jsx

--- frontend/src/components/Users/Person.jsx
+++ frontend/src/components/Users/Person.jsx
@@ -2,13 +2,14 @@ import React, { useEffect } from 'react'
 import toast from 'react-hot-toast'
 import axios from 'axios'
 import { IoMdAddCircleOutline } from "react-icons/io";
+import { useAuthContext } from '../../context/AuthContext';
 
 
 
 const Person = ({lastIndex,item}) => {
   
   const token = localStorage.getItem("token")
-  const url = "http://localhost:4000"
+  const {url} = useAuthContext()
   const image = item.image;
 
   const handleClick = async (e) => {
